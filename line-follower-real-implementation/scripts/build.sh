1bd09bba3102aff3816daea44ba7aa7b640cd254
Dev Kumar <contact@dev-kumar.com>
2026-09-13 18:28:34 +0200

    Make UR7e build non-destructive

--- line-follower-real-implementation/scripts/build.sh
+++ line-follower-real-implementation/scripts/build.sh
@@ -2,5 +2,4 @@
 set -Eeo pipefail
 source "$(dirname "$0")/common.sh"
 cd "$ROOT/ros2_ws"
-rm -rf build/ur7e_visual_rl_demo install/ur7e_visual_rl_demo
 colcon build --symlink-install --packages-select ur7e_visual_rl_demo
